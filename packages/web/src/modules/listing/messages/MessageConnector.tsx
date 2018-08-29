b5ce5ccc073edf5ece62a18455a064e357e0d663
klevecz <arielklevecz@gmail.com>
2018-08-29 12:47:26 +0200

    the boys

---
+++ packages/web/src/modules/listing/messages/MessageConnector.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { ViewMessages } from "@abb/controller";
+import { InputBar } from './InputBar';
+
+export class MessageConnector extends React.PureComponent<RouteComponentProps<{
+    listingId: string
+}>> {
+
+    unsubscribe: () => void;
+
+    render() {
+        const {
+            match: {
+                params: { listingId }
+            }
+        } = this.props;
+        return (
+            <ViewMessages listingId={listingId}>
+            {({loading,messages,subscribe}) => {
+                if (loading) {
+                return <div>...loading</div>
+                }
+
+                if (!this.unsubscribe) {
+                    this.unsubscribe = subscribe();
+                }
+
+                return (<div>
+                        {messages.map((m,i) => (
+                        <div key={`${i}-lm`}>{m.text}</div>
+                        ))}
+                        <InputBar listingId={listingId}/>
+                        </div>
+                        );
+            }}
+            </ViewMessages>
+        );
+    }
+}
