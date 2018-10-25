212cc286e4590ec6a753c3b4510b94d64e37103c
traysimay <traysimay@rumormill.fyi>
2018-10-24 20:55:21 -0700

    pdf madness

--- packages/web/src/modules/listing/grab/GrabTicketConnector.tsx
+++ packages/web/src/modules/listing/grab/GrabTicketConnector.tsx
@@ -37,12 +37,9 @@ class C extends React.PureComponent<
         {this.state.ticketId && 
         <div id="ticket-grab-container" style={{height:"100%"}}>
             {this.state.fileType==="pdf" ?
-            <object data={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`} type="application/pdf" width="100%" height="100%">
-            <p>Your web browser doesn't have a PDF plugin.
-            Instead you can <a href={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`}>click here to
-            download the PDF file.</a></p>
-                </object>
-                // <embed type="application/pdf" id="ticket-grab" src={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`} />
+            <div style={{textAlign:"center"}}>
+            <a style={{color:"red",fontFamily:"IBM Plex Sans",fontSize:"30px"}} href={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`}>DOWNLOAD</a>
+            <embed src={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`} type="application/pdf" width="100%" height="100%"/>            </div>
                 :
                 <img id="ticket-grab" src={`https://s3-us-west-1.amazonaws.com/last-minute-ticket/${this.state.ticketId}`} />
             }
