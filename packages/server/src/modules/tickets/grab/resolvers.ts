72a2b89e140e052351959414bb57ab2eba21c3cb
traysimay <traysimay@rumormill.fyi>
2018-10-25 20:24:14 -0700

    grab logic

--- packages/server/src/modules/tickets/grab/resolvers.ts
+++ packages/server/src/modules/tickets/grab/resolvers.ts
@@ -2,54 +2,33 @@ import { ResolverMap } from "../../../types/graphql-utils";
 import { Ticket } from "../../../entity/Ticket";
 import { isAuthenticated } from "../../shared/isAuthenticated";
 import { finderDefaultId,ticketCacheKey } from "../../../constants";
-import { getConnection } from "typeorm";
 
 export const resolvers: ResolverMap = {
   Mutation: {
     grabTicket: async (_, { listingId }, { session,redis }) => {
       await isAuthenticated(session);
 
-      // should probably reorg this, it assumes there can be multiple tickets, though perhaps all these checks function
-      console.log('grab');
-      const cheater = await Ticket.findOne({where:{listingId,finderId:session.userId}});
-
-      console.log(cheater);
-      if (cheater){
-        return {ticketId:cheater.filename};
-      }
-
-      // this is built to get a ticket from a listing that could have more than one ticket
-      const ticketQB = await getConnection()
-      .getRepository(Ticket)
-      .createQueryBuilder("t")  
-      .andWhere("t.listingId = :listingId",{listingId})    
-      .andWhere("t.finderId = :finderDefaultId",{finderDefaultId});
-      
-      // this is breaking when this query returns nothing and the finderId is attempted to be set
-      const availTick = await ticketQB.take(1).getMany();
-      console.log(availTick);
-      if (availTick.length===0) {
-        console.log('whart')
+      const availTicket = await Ticket.findOne({where:{listingId, finderId:finderDefaultId}});
+      if (session.userId && availTicket){
+        availTicket.finderId = session.userId;
+        await availTicket.save();
+        const tickets = await redis.lrange(ticketCacheKey, 0, -1);
+        await redis.del(ticketCacheKey);
+        const newTix = tickets.filter((x:string) => JSON.parse(x).tid !== availTicket.id);
+        if ( newTix.length > 0 ) {
+          await redis.lpush(ticketCacheKey, ...newTix);
+        }
+      } else {
+        console.log("ticker has already been found, checking user...")
+        const Ticky = await Ticket.findOne({where:{listingId, finderId:session.userId}});
+        if (Ticky){
+          return {ticketId:Ticky.filename};
+        }
         return {ticketId:"gone"};
       }
-      if (session.userId){
-        availTick[0].finderId = session.userId;
-      }
-      await availTick[0].save();
-
-      const tickets = await redis.lrange(ticketCacheKey, 0, -1);
-      await redis.del(ticketCacheKey);
-      console.log(availTick[0].id)
-      const newTix = tickets.filter((x:string) => JSON.parse(x).tid !== availTick[0].id);
-      if ( newTix.length > 0 ) {
-        console.log('WHY PUSH');
-        await redis.lpush(ticketCacheKey, ...newTix);
-        console.log(await redis.lrange(ticketCacheKey, 0, -1));
-      }
-
-
       // checking if there is actually a ticket that belngs to the user
       const theTicket = await Ticket.findOne({where:{listingId,finderId:session.userId}});
+      console.log('THE TICKET RECHECK', theTicket);
       if (theTicket){
         return {ticketId:theTicket.filename};
       }
